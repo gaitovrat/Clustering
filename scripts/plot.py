b4c6811d697040c3a13dc746ad9777d01df8e4a5
Ratmir Gaitov <gaitovrat@gmail.com>
2023-05-08 18:50:40 +0200

    Update script to show clusters

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,21 +1,27 @@
 from sys import argv
+from typing import Optional
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot(filepath: str, xname: str, yname: str):
+def plot(filepath: str, xname: str, yname: str, sname: Optional[str] = None):
     csv = pd.read_csv(filepath)
+    labels = None
+
+    if sname is not None:
+        labels = csv.loc[:, sname]
 
     plt.xlabel(xname)
     plt.ylabel(yname)
-    plt.scatter(csv.loc[:, xname], csv.loc[:, yname])
+    scatter = plt.scatter(csv.loc[:, xname], csv.loc[:, yname], c=labels)
+    plt.legend(*scatter.legend_elements())
     plt.show()
 
 
 if __name__ == "__main__":
     if len(argv) < 4:
-        print("Usage:\n\tplot.py filepath xname yname")
+        print("Usage:\n\tplot.py filepath xname yname [sname]")
         exit(1)
 
     plot(*argv[1:])
